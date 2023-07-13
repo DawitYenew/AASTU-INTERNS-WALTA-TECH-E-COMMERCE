0772bf9bb99511d5b9ead8fc180d347042eb4a5c
DawitYenew <wizdevaxo.06464@gmail.com>
2023-07-13 20:12:20 +0300

    comments added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//this is the root file
+//
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
